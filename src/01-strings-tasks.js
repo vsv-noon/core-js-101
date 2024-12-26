57ec1c0d09dc1d36423efa8872e5741ae618db2b
ViacheslavAnisimov <forvsn@gmail.com>
2024-12-26 16:54:25 +0700

    feat: solved new string-tasks

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -142,7 +142,8 @@ function removeFirstOccurrences(str, value) {
  *   '<a>' => 'a'
  */
 function unbracketTag(str) {
-  return str.replace(/[<>]/g, '');
+  // return str.replace(/[<>]/g, '');
+  return str.slice(1, -1);
 }
 
 
@@ -156,8 +157,8 @@ function unbracketTag(str) {
  *   'Thunderstruck' => 'THUNDERSTRUCK'
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
-function convertToUpperCase(/* str */) {
-  throw new Error('Not implemented');
+function convertToUpperCase(str) {
+  return str.toUpperCase();
 }
 
 /**
@@ -175,8 +176,8 @@ function convertToUpperCase(/* str */) {
  *   ],
  *   'info@gmail.com' => ['info@gmail.com']
  */
-function extractEmails(/* str */) {
-  throw new Error('Not implemented');
+function extractEmails(str) {
+  return str.split(';');
 }
 
 /**
