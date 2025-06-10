576c953ca3b7c73f745f9d45f1377e07789e7a1b
vsv-noon <forvsn@gmail.com>
2025-06-10 09:21:12 +0700

    feat: solved new conditions-n-loops-tasks

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -251,8 +251,8 @@ function reverseString(str) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  return Number(num.toString().split('').reverse().join(''));
 }
 
 /**
@@ -275,8 +275,31 @@ function reverseInteger(/* num */) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(ccn) {
+  const num = ccn.toString().split('');
+  let sum = 0;
+
+  if (num.length % 2 !== 0) {
+    for (let i = 0; i < num.length; i += 1) {
+      if (i % 2 !== 0) {
+        sum += +num[i] * 2 > 9 ? +num[i] * 2 - 9 : +num[i] * 2;
+      } else {
+        sum += +num[i];
+      }
+    }
+    return sum % 10 === 0;
+  }
+  if (num.length % 2 === 0) {
+    for (let i = 0; i < num.length; i += 1) {
+      if (i % 2 === 0) {
+        sum += +num[i] * 2 > 9 ? +num[i] * 2 - 9 : +num[i] * 2;
+      } else {
+        sum += +num[i];
+      }
+    }
+    return sum % 10 === 0;
+  }
+  return -1;
 }
 
 /**
@@ -293,8 +316,12 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  const sum = num.toString().split('').reduce((acc, curr) => acc + Number(curr), 0);
+  if (sum > 9) {
+    return sum.toString().split('').reduce((acc, curr) => acc + Number(curr), 0);
+  }
+  return sum;
 }
 
 /**
